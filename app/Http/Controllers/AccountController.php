dfda678ef63141e71a9a02c39baec9e3c052c424
Ragib Shahriar <ragibshahriar58@gmail.com>
2023-12-27 20:57:52 +0600

    User Authentication

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
+use Illuminate\Support\Facades\Auth;
+use function Laravel\Prompts\password;
+
 class AccountController extends Controller
 {
     //This method will show user registration page
@@ -46,5 +49,33 @@ class AccountController extends Controller
     //This method will show user login page
     public function login() {
         return view('front.account.login');
+    }
+
+    public function authenticate(Request $request){
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->passes()) {
+              
+            if(Auth::attempt(['email'=> $request->email, 'password'=> $request->password])){
+                return redirect()->route('account.profile');
+            }
+            else {
+                return redirect()->route('account.login')->with('error','Either Email/Password is incorrect');
+            }
+        } else {
+            return redirect()->route('account.login')->withErrors($validator)->withInput($request->only('email'));
+        }
+    }
+
+    public function profile() {
+        return view('front.account.profile');
+    }
+
+    public function logout() {
+        Auth::logout();
+        return redirect()->route('account.login');
     }
 }
